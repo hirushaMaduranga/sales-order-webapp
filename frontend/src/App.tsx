897efd3e2826d7f926439f154278a6d6ac830439
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-04-26 16:34:26 +0530

    edit app.tsx file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/sales-order" element={<SalesOrderPage />} />
+      <Route path="/sales-order/new" element={<SalesOrderPage />} />
+      <Route path="/sales-order/:orderId" element={<SalesOrderPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
