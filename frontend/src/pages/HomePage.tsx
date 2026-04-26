74a4df27a97d1c5287fcbc3f25f043ff4da157a9
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-04-26 15:26:34 +0530

    HomePage.tsx file

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom'
+import { Button } from '../components/Button'
+import { PageShell } from '../components/PageShell'
+import { TableGrid } from '../components/TableGrid'
+
+const headers = ['Col 1', 'Col 2', 'Col 3', 'Col 4', 'Col 5', 'Col 6', 'Col 7']
+
+export function HomePage() {
+  const navigate = useNavigate()
+
+  return (
+    <PageShell
+      title="Home"
+      action={
+        <Button type="button" onClick={() => navigate('/sales-order')}>
+          Add New
+        </Button>
+      }
+    >
+      <TableGrid headers={headers} rows={8} />
+    </PageShell>
+  )
+}
