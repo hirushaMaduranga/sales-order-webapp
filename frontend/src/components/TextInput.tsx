ce2c7bfb133996967def3c890b7c188e87a37f8f
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-04-26 15:25:14 +0530

    Textinput.tsx file

---
+++ frontend/src/components/TextInput.tsx
@@ -0,0 +1,9 @@
+import type { InputHTMLAttributes } from 'react'
+
+const baseClassName = 'h-8 w-full border border-slate-700 bg-white px-2 text-sm'
+
+type TextInputProps = InputHTMLAttributes<HTMLInputElement>
+
+export function TextInput({ className = '', ...props }: TextInputProps) {
+  return <input className={`${baseClassName} ${className}`.trim()} {...props} />
+}
