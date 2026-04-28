3ecdde93fc9a1315eef4c6876088946a520968c9
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-04-28 21:55:20 +0530

    HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,13 +1,23 @@
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button } from '../components/common/Button'
 import { PageShell } from '../components/common/PageShell'
 import { OrdersTable } from '../components/home/OrdersTable'
-import { useAppSelector } from '../hooks/storeHooks'
+import { fetchSalesOrders } from '../features/orders/ordersSlice'
+import { useAppDispatch, useAppSelector } from '../hooks/storeHooks'
 
 export function HomePage() {
+  const dispatch = useAppDispatch()
   const navigate = useNavigate()
   const orders = useAppSelector((state) => state.orders.orders)
+  const ordersStatus = useAppSelector((state) => state.orders.status)
   const rows = 8
+
+  useEffect(() => {
+    if (ordersStatus === 'idle') {
+      dispatch(fetchSalesOrders())
+    }
+  }, [dispatch, ordersStatus])
 
   return (
     <PageShell
