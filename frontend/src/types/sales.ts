6712591cbc659b7b6ec0b113fa0f6b7cf17227fc
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-04-28 21:56:00 +0530

    sales.ts file

--- frontend/src/types/sales.ts
+++ frontend/src/types/sales.ts
@@ -9,12 +9,30 @@ export type Customer = {
   postCode: string
 }
 
+export type ApiClientDto = {
+  clientId: number
+  customerName: string
+  address1: string
+  address2: string | null
+  address3: string | null
+  suburb: string
+  state: string
+  postCode: string
+}
+
 export type ItemCatalogEntry = {
   id: string
   code: string
   description: string
   price: number
   defaultTaxRate: number
+}
+
+export type ApiItemDto = {
+  itemId: number
+  itemCode: string
+  description: string
+  unitPrice: number
 }
 
 export type LineItem = {
@@ -26,6 +44,20 @@ export type LineItem = {
   quantity: number
   price: number
   taxRate: number
+}
+
+export type ApiSalesOrderLineResponseDto = {
+  salesOrderLineId: number
+  itemId: number
+  itemCode: string
+  description: string
+  lineNote: string | null
+  quantity: number
+  price: number
+  taxRate: number
+  exclAmount: number
+  taxAmount: number
+  inclAmount: number
 }
 
 export type SalesOrder = {
@@ -43,4 +75,21 @@ export type SalesOrder = {
   referenceNo: string
   note: string
   lineItems: LineItem[]
+}
+
+export type ApiSalesOrderResponseDto = {
+  salesOrderId: number
+  orderNo: string
+  clientId: number
+  customerName: string
+  invoiceNo: string
+  invoiceDate: string
+  referenceNo: string | null
+  note: string | null
+  state: string
+  postCode: string
+  totalExcl: number
+  totalTax: number
+  totalIncl: number
+  lines: ApiSalesOrderLineResponseDto[]
 }
